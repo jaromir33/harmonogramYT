bda6d774ad53775e3d900ce0c79dcd052bc6430f
jaromir33 <jaromir33@gmail.com>
2026-05-05 15:59:50 +0200

    Konfiguracja wiki v0.81

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jaromir33.github.io/harmonogramYT",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
